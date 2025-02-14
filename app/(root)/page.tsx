e551f2d3f678879223d19590d13dac06730d7704
MohitRanjan2433 <126330385+MohitRanjan2433@users.noreply.github.com>
2025-02-14 16:52:09 +0530

    .

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,12 @@ import { auth, signOut } from "@/auth";
 
 
 const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
 
   return(
   <>
-    <h1>Welcome to NextJS</h1>  
+     <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items">
+      All Questions
+     </section>
   </>
 )
 };
